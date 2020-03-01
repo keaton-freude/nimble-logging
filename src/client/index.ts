0b49a113c28cd7f3db709acd53f46f3d4f1dc531
Keaton Freude <freudek90@gmail.com>
2020-03-01 02:30:54 -0800

    Add browserify. Fix exports for entire project

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,1 +1,3 @@
 export * from "./client";
+export * from "./log-formatter";
+export * from "./log-level";
---
+++ src/sink/index.ts
@@ -0,0 +1,5 @@
+export * from "./file-sink";
+export * from "./sink-factory";
+export * from "./sink";
+export * from "./websocket-listener";
+export * from "./websocket-sink";
