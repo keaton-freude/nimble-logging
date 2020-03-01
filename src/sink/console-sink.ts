c709f48f473682782844e3ad349fe8dbc911ed84
Keaton Freude <freudek90@gmail.com>
2020-03-01 11:01:02 -0800

    Add ctor to ConsoleSink

--- src/sink/console-sink.ts
+++ src/sink/console-sink.ts
@@ -2,6 +2,7 @@ import { ILoggerSink } from "./sink";
 
 // Writes out to the console, like a typical `console.log`
 export class ConsoleSink implements ILoggerSink {
+    constructor() {}
     async SinkLog(message: string): Promise<void> {
         console.log(message);
     }
